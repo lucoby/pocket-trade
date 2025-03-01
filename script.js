8e228c7af8e1590adccbd9986e8e5959f052852d
lucoby <lucoby@gmail.com>
2025-03-01 14:29:13 -0500

    Add toggleable filters

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 $(document).ready(function () {
-    cardFiles = [
-        'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A1.json',
-        // 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A1a.json',
-        // 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A2.json',
-        // 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A2a.json',
-        // 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/P-A.json'
-    ];
-    let listItems = '';
-    cardFiles.forEach(file => {
-        $.getJSON(file, function (data) {
-            $.each(data, function (index, item) {
-                listItems += `<li><p>${item.rarity} - ${item.name}</p><img src="${item.image}" alt="${item.name}" width="100"></li>`;
-            });
-            $('#cardsContainer').html(listItems);
-        }).fail(function () {
-            console.error('Error loading JSON file');
+  const cardFiles = {
+    'A1': 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A1.json',
+    'A1a': 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A1a.json',
+    'A2': 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A2.json',
+    'A2a': 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/A2a.json',
+    // 'P-A': 'https://raw.githubusercontent.com/lucoby/pocket-trade/refs/heads/main/cards/P-A.json'
+  };
+  $.each(cardFiles, (key, file) => {
+    let cards = '';
+    $.getJSON(file, data => {
+        $.each(data, (index, item) => {
+          cards += `<div class="col-2 border-0 card  ${item.rarity}"><img src="${item.image}" class="mx-auto d-block" alt="${item.name}" width="170"></div>`;
         });
+        $('#cardsContainer' + key).html(cards);
+    }).fail(function () {
+      console.error('Error loading JSON file');
     });
-    
+  });
+
+  $('.filter-button').click(function() {
+      var filterValue = $(this).data('filter');
+      $(this).toggleClass('active');
+      $('.' + filterValue).toggleClass('d-none');
+      // $('.' + filterValue).hide();
+    });
 });
